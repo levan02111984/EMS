f757fc3d015597ecc84ef793f8b15eca8790148d
levan021111984@gmail.com <Nwz-a728@>
2024-11-02 13:00:18 +0700

    Commit

--- obj/Debug/net8.0/EMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/EMS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aff0d2a69cfb5f1f2b105e7a4780305ea3096ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
