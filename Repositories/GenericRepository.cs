ef3cf33c9be298c57c38bd46297ea2be9d2f3812
levan021111984@gmail.com <Nwz-a845@>
2024-11-05 10:04:34 +0700

    Apply GenericRepository Pattern

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using EMS.Data;
+
+namespace EMS.Repositories
+{
+
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task DeleteAsync(object Id)
+        {
+            var entity = await _dbSet.FindAsync(Id);
+            if (entity != null)
+            {
+                {
+                    _dbSet.Remove(entity);
+                    await _context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object Id)
+        {
+            return await _dbSet.FindAsync(Id);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity); // Mark the entity state as modified/
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EMS.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+
+        //Retrieves a single instance of type T based on the provided ID.It returns null if no entity is found, which is indicated by T?.
+        Task<T?> GetByIdAsync(object id);
+
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(object id);
+        
+        Task SaveAsync(); // Committing all changes from context into the database
+        void Dispose(); // Clear unsued resources
+    }
+}
+
