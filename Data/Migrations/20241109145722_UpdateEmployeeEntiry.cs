d75f9a340579ebc773d4d9d20243e415b848a097
levan021111984@gmail.com <Nwz-a845@>
2024-11-10 15:51:45 +0700

    Update DBContext

---
+++ Data/Migrations/20241109145722_UpdateEmployeeEntiry.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployeeEntiry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_CountryID",
+                table: "Employees",
+                column: "CountryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentID",
+                table: "Employees",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DesignationID",
+                table: "Employees",
+                column: "DesignationID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Countries_CountryID",
+                table: "Employees",
+                column: "CountryID",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentID",
+                table: "Employees",
+                column: "DepartmentID",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Designations_DesignationID",
+                table: "Employees",
+                column: "DesignationID",
+                principalTable: "Designations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Countries_CountryID",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentID",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Designations_DesignationID",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_CountryID",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentID",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DesignationID",
+                table: "Employees");
+        }
+    }
+}
---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EMS.Models;
+using EMS.Repositories;
+namespace EMS.Data
+{
+    public class UnitOfWork : IDisposable
+    {
+        private ApplicationDbContext appDbContext = new Data.ApplicationDbContext();
+
+        private GenericRepository<Department> departmentRepository;
+        private GenericRepository<Employee> employeeRepository;
+
+
+        //Singleton parttern
+        public GenericRepository<Department> DepartmentRepository
+        {
+            get
+            {
+                if(this.departmentRepository == null)
+                {
+                    this.departmentRepository = new GenericRepository<Department>(appDbContext);
+                }
+                return this.departmentRepository;
+            }
+        }
+
+        //Singleton parttern
+        public GenericRepository<Employee> EmployeeRepository
+        {
+            get
+            {
+                if (this.employeeRepository == null)
+                {
+                    this.employeeRepository = new GenericRepository<Employee>(appDbContext);
+                }
+                return this.employeeRepository;
+            }
+        }
+      
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    appDbContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
