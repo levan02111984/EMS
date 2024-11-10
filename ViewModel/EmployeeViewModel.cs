bbdbb3b9ee5c7d31de330e663ede785e8dc57f86
levan021111984@gmail.com <Nwz-a845@>
2024-11-10 15:53:05 +0700

    Add EmployeeViewModel

---
+++ ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EMS.Models;
+
+namespace EMS.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public EmployeeViewModel() { }
+        public Employee Employee { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string CountryName { get; set; }
+
+        public string DesignationName { get; set; }
+
+    }
+}
